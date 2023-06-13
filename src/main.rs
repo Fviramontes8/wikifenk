04f6a1b52297f1984db7ef06b5ec9b1eaf958735
Fviramontes8 <kuolemajarvi@yahoo.com>
2023-06-12 23:37:53 -0600

    Add init project files
    Add initial project files, a website is lauched via Rocket
    
    The install pages are mostly written while the API ones are more
    placeholders and will be completed at a later time

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+#[macro_use]
+extern crate rocket;
+
+#[macro_use]
+extern crate rocket_dyn_templates;
+
+use rocket_dyn_templates::Template;
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", context! {})
+}
+
+#[get("/")]
+fn tensorflow_docs() -> Template {
+    Template::render("tensorflow", context! {})
+}
+
+#[get("/")]
+fn tensorflowlite_docs() -> Template {
+    Template::render("tensorflowlite", context! {})
+}
+
+#[get("/")]
+fn opencv_install() -> Template {
+    Template::render("opencv_install", context! {})
+}
+
+#[get("/")]
+fn tflite_install() -> Template {
+    Template::render("tflite_install", context! {})
+}
+
+#[get("/")]
+fn cmake_install() -> Template {
+    Template::render("cmake_install", context! {})
+}
+
+#[get("/")]
+fn render_css() -> Template {
+    Template::render("style", context! {})
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/tensorflow", routes![tensorflow_docs])
+        .mount("/tensorflowlite", routes![tensorflowlite_docs])
+        .mount("/opencv_install", routes![opencv_install])
+        .mount("/tflite_install", routes![tflite_install])
+        .mount("/cmake_install", routes![cmake_install])
+        .mount("/style.css", routes![render_css])
+        .attach(Template::fairing())
+}
